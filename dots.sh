27ea8f80adfa50aed5a2905e14414852d631ed67
Bongardino <bongardino@gmail.com>
2020-06-26 10:50:32 -0400

    catchup

--- dots.sh
+++ dots.sh
@@ -6,7 +6,7 @@
 backup_dir="$HOME"/Desktop/dot-backup
 git_dir="$HOME"/src/dotfiles
 
-function dot_git {
+function dot-git {
    /usr/bin/git --git-dir="$git_dir" --work-tree="$HOME" "$@"
 }
 
@@ -14,16 +14,16 @@ mkdir -p "$backup_dir"
 
 # git init --bare "$git_dir"
 git clone --bare https://github.com/bongardino/dotfiles.git "$git_dir"
-dot_git checkout
+dot-git checkout
 
 if [ $? = 0 ]; then
   echo "Checked out dots.";
   else
     echo "Backing up pre-existing dots and retrying ...";
-    dot_git checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I {} mv $HOME/{} $backup_dir
+    dot-git checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I {} mv $HOME/{} $backup_dir
     mv $git_dir $backup_dir || true
 	git clone --bare https://github.com/bongardino/dotfiles.git "$git_dir"
-    dot_git checkout
+    dot-git checkout
 fi;
 
-dot_git config status.showUntrackedFiles no
+dot-git config status.showUntrackedFiles no
